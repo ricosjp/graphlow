eaaccbe8b4ad7b6ea2e8ef5bc0a6dea3d78e41e6
horiem <yellowshippo@gmail.com>
2024-07-06 06:36:23 +0900

    add tests

--- tests/test_base/test_mesh.py
+++ tests/test_base/test_mesh.py
@@ -8,104 +8,17 @@ import graphlow
 
 
 @pytest.mark.parametrize(
-    "file_name, desired",
+    "file_name, desired_file",
     [
         (
-            pathlib.Path("tests/data/vtk/hex/mesh.vtk"),
-            np.array([
-                [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
-                [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-            ]),
-        ),
-        (
-            pathlib.Path("tests/data/vtu/mix_poly/mesh.vtu"),
-            np.array([
-                # 0  1  2  3  4  5  6  7  8  9 10 11 12
-                [1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0],
-                [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
-            ]),
+            pathlib.Path("tests/data/vtu/complex/mesh.vtu"),
+            pathlib.Path("tests/data/vtu/complex/surface.vtu"),
         ),
     ],
 )
-def test__compute_point_cell_incidence(file_name, desired):
+def test__convert_to_surface(file_name, desired_file):
     mesh = graphlow.read(file_name)
-    cell_point_incidence = mesh.compute_cell_point_incidence()
-    np.testing.assert_array_equal(
-        cell_point_incidence.toarray().astype(int), desired)
-
-
-@pytest.mark.parametrize(
-    "file_name, desired",
-    [
-        (
-            pathlib.Path("tests/data/vtk/hex/mesh.vtk"),
-            np.array([
-                [1, 1],
-                [1, 1],
-            ]),
-        ),
-        (
-            pathlib.Path("tests/data/vtu/mix_poly/mesh.vtu"),
-            np.array([
-                [1, 1, 1],
-                [1, 1, 0],
-                [1, 0, 1],
-            ]),
-        ),
-    ],
-)
-def test__compute_cell_adjacency(file_name, desired):
-    mesh = graphlow.read(file_name)
-    cell_adjacency = mesh.compute_cell_adjacency()
-    np.testing.assert_array_equal(
-        cell_adjacency.toarray().astype(int), desired)
-
-
-@pytest.mark.parametrize(
-    "file_name, desired",
-    [
-        (
-            pathlib.Path("tests/data/vtk/hex/mesh.vtk"),
-            np.array([
-                # 0  1  2  3  4  5  6  7  8  9 10 11
-                [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],  #  0
-                [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],  #  1
-                [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],  #  2
-                [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],  #  3
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],  #  4
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],  #  5
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],  #  6
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],  #  7
-                [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],  #  8
-                [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],  #  9
-                [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],  # 10
-                [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],  # 11
-            ]),
-        ),
-        (
-            pathlib.Path("tests/data/vtu/mix_poly/mesh.vtu"),
-            np.array([
-                # 0  1  2  3  4  5  6  7  8  9 10 11 12
-                [1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0], #  0
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], #  1
-                [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0], #  2
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], #  3
-                [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0], #  4
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], #  5
-                [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0], #  6
-                [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0], #  7
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], #  8
-                [1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1], #  9
-                [1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1], # 10
-                [1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1], # 11
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1], # 12
-            ]),
-        ),
-    ],
-)
-def test__compute_point_adjacency(file_name, desired):
-    mesh = graphlow.read(file_name)
-    point_adjacency = mesh.compute_point_adjacency()
-    np.testing.assert_array_equal(
-        point_adjacency.toarray().astype(int), desired)
+    surface = mesh.convert_to_surface()
+    desired = graphlow.read(desired_file)
+    np.testing.assert_almost_equal(surface.mesh.points, desired.mesh.points)
+    np.testing.assert_array_equal(surface.mesh.cells, desired.mesh.cells)
