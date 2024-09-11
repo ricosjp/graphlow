9f46c6a8325f79f9c5114fc88e3601a671fb8086
Daichi Ishida <ishida@ricos.co.jp>
2024-09-11 13:30:04 +0900

    fix logging config

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,27 @@
 import logging.config
 
-from .logging_config import LOGGING_CONFIG
+LOGGING_CONFIG = {
+    "version": 1,
+    "loggers": {
+        "": {  # root
+            "level": "INFO",
+            "handlers": ["console_handler"],
+        },
+    },
+    "handlers": {
+        "console_handler": {
+            "class": "logging.StreamHandler",
+            "level": "DEBUG",
+            "formatter": "simpleFormatter",
+            "stream": "ext://sys.stdout",
+        },
+    },
+    "formatters": {
+        "simpleFormatter": {
+            "format": "%(asctime)s | %(levelname)s | %(name)s | [%(filename)s:%(lineno)d] %(message)s"
+        },
+    },
+}
 
 logging.config.dictConfig(LOGGING_CONFIG)
 
