81855b8e251fb85a9d35b8bc0480fabf35dc586b
Daichi Ishida <ishida@ricos.co.jp>
2024-11-25 21:24:47 +0900

    add benchmark

---
+++ tests/benchmark/test_time.py
@@ -0,0 +1,41 @@
+import pathlib
+from collections.abc import Generator
+
+import pytest
+
+import graphlow
+from graphlow.util.logger import get_logger
+
+logger = get_logger(__name__)
+
+
+@pytest.mark.with_benchmark
+@pytest.mark.parametrize(
+    "file_name",
+    sorted(
+        pathlib.Path("tests/data/benchmark_mesh/surfaces").glob("100x100_*.vtu")
+    ),
+)
+def test_compute_areas_100x100_benchmark(
+    benchmark: Generator, file_name: pathlib.Path
+):
+    surfmesh = graphlow.read(file_name)
+    def compute_areas():
+        _ = surfmesh.compute_areas()
+    benchmark(compute_areas)
+
+
+@pytest.mark.with_benchmark
+@pytest.mark.parametrize(
+    "file_name",
+    sorted(
+        pathlib.Path("tests/data/benchmark_mesh/volumes").glob("100x100_*.vtu")
+    ),
+)
+def test_compute_volumes_100x100_benchmark(
+    benchmark: Generator, file_name: pathlib.Path
+):
+    volmesh = graphlow.read(file_name)
+    def compute_volumes():
+        _ = volmesh.compute_volumes()
+    benchmark(compute_volumes)
