be6a51996f76587756bb5f83df00f318f155a9f1
Daichi Ishida <ishida@ricos.co.jp>
2025-02-26 10:44:45 +0900

    setup sphinx

---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+graphlow : Graph for Physics
+============================
+
+*graphlow* provides graph processing features for physical simulation data (e.g., mesh).
+
+
+.. toctree::
+   :maxdepth: 2
+
+   ./quickstart/index
+   ./tutorials/index
+   ./modules/modules
+   ./changelog
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- src/graphlow/__init__.py
+++ src/graphlow/__init__.py
@@ -3,3 +3,11 @@ from graphlow.base.mesh import GraphlowMesh
 from graphlow.base.tensor import GraphlowTensor
 from graphlow.io.io import read
 from graphlow.util.typing import ArrayDataType
+
+try:
+    from importlib.metadata import version
+except ImportError:  # Python < 3.8
+    from pkg_resources import get_distribution as version
+
+__version__ = version("graphlow")
+__all__ = ["__version__"]
